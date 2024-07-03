e7bf4a6ebb515d1a478126701344fa89d49fca3e
Andy Lindsay <andydlindsay@gmail.com>
2024-07-02 18:11:40 -0700

    updates readme

--- m05w12-1/command-line/index.js
+++ m05w12-1/command-line/index.js
@@ -4,11 +4,11 @@ const Client = pg.Client; // one connection to the rdbms
 // const Pool = pg.Pool; // collection of clients (5); managed
 
 const config = {
-  host: 'salt.db.elephantsql.com',
-  port: 5432,
-  database: 'ejvaiama',
-  user: 'ejvaiama',
-  password: '1r5PEN8JLab5XwLmTJhr7CFCnk9soH0L'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
 };
 
 const client = new Client(config); // new Pool(config)
--- m05w12-1/web/database/connection.js
+++ m05w12-1/web/database/connection.js
@@ -3,14 +3,6 @@ const pg = require('pg');
 const Client = pg.Client; // one connection to the rdbms
 // const Pool = pg.Pool; // collection of clients (5); managed
 
-// const config = {
-//   host: 'salt.db.elephantsql.com',
-//   port: 5432,
-//   database: 'ejvaiama',
-//   user: 'ejvaiama',
-//   password: '1r5PEN8JLab5XwLmTJhr7CFCnk9soH0L'
-// };
-
 const config = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
